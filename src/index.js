01f1c1d9c5e5773e19d2045d4d11a4ec4ffad18d
mills-git <stefmills03@gmail.com>
2026-03-09 23:49:50 +0000

    Add import for `fs`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as fs from "fs"
 
 try {
     const codeIssuesFilePath = core.getInput("code-issues-file-path");
